d23dd8e67ee9e266724d3818d1b72441b9df2483
LingDian <857388137@qq.com>
2022-01-13 15:13:38 +0800

    保存

--- src/IEAutoRefresh.ConsoleApp/Program.cs
+++ src/IEAutoRefresh.ConsoleApp/Program.cs
@@ -1,11 +1,11 @@
 ﻿using SHDocVw;
 using Spectre.Console;
 
-Console.Title = $"调用IE打开网页，并静默刷新";
+Console.Title = $"【作者：零点 邮箱：857388137@qq.com】 -> 调用IE打开网页，并静默刷新";
 AnsiConsole.Write(new FigletText("LingDian").Centered().Color(Color.Red));
 string url = AnsiConsole.Ask<string>("[green]请输入要刷新的网页地址：[/]");
-Console.Title = $"调用IE打开网页，并静默刷新：{url}";
-int timeInterval  = AnsiConsole.Ask<int>("[green]请输入要定时刷新时间，单位毫秒：[/]");
+int timeInterval = AnsiConsole.Ask<int>("[green]请输入要定时刷新时间，单位毫秒：[/]");
+
 InternetExplorer ie = new();
 ie.Navigate(url);
 ie.Visible = true;
